907966dd209b0972c8877d42c24170b70f2a6187
William Miles <willmiles@live.co.uk>
2019-10-21 11:58:52 +0100

    available products added to user interface and npm dependacies adjusted

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const { checkout, possibleItemsArr } = require('./utils/utils.js');
 const scannedItems = [];
 
 console.log(
-  'Welcome to the checkout.\n\nPlease scan your first item by typing in the code and pressing `enter`.\n\nWhen you have finished scanning press `.`'
+  `Welcome to the checkout.\n\nPlease scan your first item by typing in the code and pressing 'enter'.
+The availabe items are: ${possibleItemsArr}\n\nWhen you have finished scanning input '.'`
 );
 
 standard_input.on('data', function(data) {
@@ -18,7 +19,7 @@ standard_input.on('data', function(data) {
     if (possibleItemsArr.indexOf(data[0]) !== -1) {
       scannedItems.push(data[0]);
       console.log(
-        'Cart : \n' + scannedItems + '\nScan your next item. (`.`) to finish\n'
+        'Cart : ' + scannedItems + '\nScan your next item. (`.`) to finish\n'
       );
     } else {
       console.log('Sorry that item is not available. try again\n');
